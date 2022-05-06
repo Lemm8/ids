94480670a6b69dfcaca3d22cc4c2f6c24e8ffefa
Lemm8 <luis.meza.manriquez@hotmail.com>
2022-05-05 18:42:41 -0700

    IDS API -> Version 1.0 - Connect to JAWSDB

--- db/connection.ts
+++ db/connection.ts
@@ -1,11 +1,18 @@
 import { Sequelize } from 'sequelize';
 
+let db: Sequelize;
 
-const db = new Sequelize( process.env.DATABASE!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
-});
+if ( process.env.JAWSDB_URL ) {
+    db = new Sequelize( process.env.JAWSDB_URL );
+} else {
+    db = new Sequelize( process.env.DATABASE!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+        logging: false
+    });
+}
+
 
 
 export default db;
--- models/server.ts
+++ models/server.ts
@@ -29,7 +29,7 @@ class Server {
     constructor() {
 
         this.app = express();
-        this.port = process.env.PORT!;
+        this.port = process.env.PORT || '3301';
         
         // CONECTAR CON LA BASE DE DATOS
         this.dbConnection();
