f5a53dcd4cc6b15990e9f6286646a3223f2740f4
Lemm8 <luis.meza.manriquez@hotmail.com>
2022-09-18 21:55:21 -0700

    IDS REST API BETA 1.1.1

--- config/corsOptions.ts
+++ config/corsOptions.ts
@@ -1,13 +1,12 @@
 import allowedOrigins from "./allowedOrigins";
 
 const corsOptions = {
-    // origin: '*',
     origin: ( origin: any, callback: any ) => {
         if ( allowedOrigins.indexOf( origin ) !== -1 || !origin ) {
             callback( null, true )
         } else {
-            callback( new Error( 'Not allowed by cors' ) )
-        }
+            callback( new Error( `${origin} not allowed by cors` ) )
+        } 
     },
     optionsSuccessStatus: 200,
     credentials: true
--- models/server.ts
+++ models/server.ts
@@ -67,8 +67,10 @@ class Server {
         this.app.use( cors( corsOptions ) );
 
         this.app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "https://idslapaz.com/"); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            req.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
+            req.header("Access-Control-Allow-Headers");
+            res.setHeader("Access-Control-Allow-Origin", "https://idslapaz.com"); // update to match the domain you will make the request from
+            res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
 
