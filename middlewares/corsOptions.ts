626e6bacffeb213132d96641ee6fbebe907da8db
Lemm8 <luis.meza.manriquez@hotmail.com>
2022-09-20 13:38:25 -0700

    IDS REST API Cors Allowed Origins middleware 1.3.5 *

--- middlewares/corsOptions.ts
+++ middlewares/corsOptions.ts
@@ -1,14 +1,15 @@
 import allowedOrigins from "../config/allowedOrigins";
 
 const corsOptions = {
-    origin: ( origin: any, callback: any ) => {
-        if ( allowedOrigins.indexOf( origin! ) !== -1 || !origin ) {
-            console.log( origin );
-            callback( null, true );
-        } else {
-            callback( new Error( `${origin} not allowed` ) )
-        }
-    },
+    origin: '*',
+    // origin: ( origin: any, callback: any ) => {
+    //     if ( allowedOrigins.indexOf( origin! ) !== -1 || !origin ) {
+    //         console.log( origin );
+    //         callback( null, true );
+    //     } else {
+    //         callback( new Error( `${origin} not allowed` ) )
+    //     }
+    // },
     methods: [ "GET", "POST", "PUT", "PATCH", "DELETE" ],
     allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
     optionsSuccessStatus: 200,
